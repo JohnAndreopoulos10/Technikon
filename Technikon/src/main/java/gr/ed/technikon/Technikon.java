351538d56b119a27f0929d79177b63ce1c6c7ae0
John Andreopoulos <john.r.andreopoulos@gmail.com>
2024-09-28 10:20:19 +0300

    Add files via upload

---
+++ Technikon/src/main/java/gr/ed/technikon/Technikon.java
@@ -0,0 +1,70 @@
+package gr.ed.technikon;
+
+import gr.ed.technikon.utility.JPAUtil;
+import gr.ed.technikon.utility.UseCases;
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class Technikon {
+
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        try {
+            while (true) {
+                System.out.println("Welcome to Technikon");
+                System.out.println("1. Populate Database");
+                System.out.println("2. Register Owner with Properties");
+                System.out.println("3. Create a new Repair for Properties Owner");
+                System.out.println("4. Select Properties from owner Id");
+                System.out.println("5. Owner has to accept or not the repair");
+                System.out.println("6. Get all pending Repairs with proposed Cost and Dates");
+                System.out.println("7. Generate Report for Property-Owner");
+                System.out.println("8. Generate Report for Admin");
+                System.out.println("9.Exit");
+
+                System.out.print("Enter your choice: ");
+                choice = scanner.nextInt();
+                scanner.nextLine(); 
+
+                switch (choice) {
+                    case 1:
+                        UseCases.dataPopulation();
+                        break;
+                    case 2:
+                        UseCases.ownerWithTwoProperties();
+                        break;
+                    case 3:
+                        UseCases.repairsForPropertiesOwner(9L);
+                        break;
+                    case 4:
+                        UseCases.selectPropertiesFromOwnerById(3);
+                        break;
+                    case 5:
+                        UseCases.ownerAcceptanceOfRepairs(7L);
+                        break;
+                    case 6:
+                        UseCases.adminGetsAllPendingRepairsWithProposedCostAndDates();
+                        break;
+                    case 7:
+                        UseCases.generateOwnerReport();
+                        break;
+
+                    case 8:
+                        UseCases.generateAdminReport();
+                        break;
+                    case 9:
+                        JPAUtil.shutdown();
+                        System.out.println("EXIT");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("");
+                }
+            }
+        } finally {
+            scanner.close();
+        }
+    }
+}
---
+++ Technikon/src/test/java/gr/ed/technikon/test/PropertyServiceTest.java
@@ -0,0 +1,46 @@
+package gr.ed.technikon.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import gr.ed.technikon.Repositories.PropertyRepository;
+import gr.ed.technikon.Repositories.PropertyRepositoryInterface;
+import gr.ed.technikon.enums.PropertyType;
+import gr.ed.technikon.models.Owner;
+import gr.ed.technikon.models.Property;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+public class PropertyServiceTest {
+
+    @Mock
+    private PropertyRepositoryInterface propertyRepositoryInterface;
+
+    @InjectMocks
+    private PropertyRepository propertyRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void findById() {
+        Long propertyId = 44444L;
+        Property property = new Property();
+        property.setPropertyId(propertyId);
+        property.setPropertyType(PropertyType.MAISONETTE);
+        property.setAddress("Kleanthous 4");
+        property.setOwner(new Owner());
+
+        when(propertyRepositoryInterface.findById(propertyId)).thenReturn(Optional.of(property));
+        Property pr = new Property();
+        pr.setPropertyId(44444l);
+//		Optional<Property> pr = propertyRepository.findById(propertyId);
+
+        assertEquals(property.getPropertyId(), pr.getPropertyId());
+    }
+}
